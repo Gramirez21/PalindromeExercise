0752c4e50536a6ed6e09dac075dcd17ee444db6b
Gramirez21 <r.dannysixty@gmail.com>
2024-04-15 16:11:06 -0700

    exercise2

---
+++ PalindromeExercise/PalindromeExercise/Wordsmith.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+        public bool IsAPalindrome(string palindrome)
+        {
+            var reversed = "";
+            for (int i = palindrome.Length - 1; i >= 0; i++)
+            {
+                reversed += palindrome[i];
+            }
+
+            if (reversed == palindrome)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,3 +1,4 @@
+using PalindromeExercise;
 using System;
 using Xunit;
 
@@ -5,10 +6,19 @@ namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
-        {
+        [Theory]
 
+        [InlineData("racecar", true)]
+        [InlineData ("null", false)]
+        [InlineData("civic", true)]
+        [InlineData("bull", false)]
+        public void Test1(string word , bool expected)
+        {
+            var test = new WordSmith();
+
+            bool actual = test.IsAPalindrome(word);
+
+           Assert.Equal(expected, actual);
         }
     }
 }
